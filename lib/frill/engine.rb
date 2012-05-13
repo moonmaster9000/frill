4ca457b12b8d29c72bd49a22b6b1035a123b154e
Matt Parker <moonmaster9000@gmail.com>
2012-05-12 20:53:38 -0400

    load frills even if in subdirs

--- lib/frill/engine.rb
+++ lib/frill/engine.rb
@@ -8,18 +8,22 @@ module Frill
       require 'frill/rails'
     end
 
+    config.before_initialize do |app|
+      app.config.paths.add 'app/decorators', :eager_load => true
+    end
+
     config.to_prepare do
       if Rails.env.development? && !Rails.application.config.cache_classes
         Frill.reset!
 
-        Frill::Engine.force_load Dir["#{Frill::Engine.root}/app/frills/*"]
-        Frill::Engine.force_load Dir["#{Rails.root}/app/frills/*"]
+        Frill::Engine.force_load Dir["#{Frill::Engine.root}/app/frills/**/*"]
+        Frill::Engine.force_load Dir["#{Rails.root}/app/frills/**/*"]
       end
     end
 
     def self.force_load files
       files.each do |f| 
-        load f
+        require_dependency f
       end
     end
   end
